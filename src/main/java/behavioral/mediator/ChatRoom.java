cbfc67ef9cc6f41afc9e93bc62265952755f9706
Erwan <erwanletutour.elt@gmail.com>
2024-02-08 13:45:43 +0100

    update mediator

--- src/main/java/behavioral/mediator/ChatRoom.java
+++ src/main/java/behavioral/mediator/ChatRoom.java
@@ -4,6 +4,6 @@ import java.util.Date;
 
 public class ChatRoom {
     public static void showMessage(User user, String message){
-        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+        System.out.println(new Date() + " [" + user.getName() + "] : " + message);
     }
 }
