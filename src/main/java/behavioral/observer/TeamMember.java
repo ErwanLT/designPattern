52ce21a007b1eaab9ea11b4a0f47a765af4c9ce2
Erwan <erwanletutour.elt@gmail.com>
2024-02-15 09:29:26 +0100

    update observer

--- src/main/java/behavioral/observer/TeamMember.java
+++ src/main/java/behavioral/observer/TeamMember.java
@@ -1,8 +1,8 @@
 package behavioral.observer;
 
-public class TeamMember implements Observer{
-    private final String name;
-    private final ChatTeam chatTeam;
+class TeamMember implements Observer {
+    private String name;
+    private ChatTeam chatTeam;
 
     public TeamMember(String name, ChatTeam chatTeam) {
         this.name = name;
@@ -12,15 +12,17 @@ public class TeamMember implements Observer{
     public void joinChat() {
         chatTeam.addObserver(this);
         System.out.println(name + " a rejoint le chat d'équipe.");
+        update("Bienvenue dans le chat d'équipe !");
     }
 
     public void leaveChat() {
         chatTeam.removeObserver(this);
         System.out.println(name + " a quitté le chat d'équipe.");
+        update("Vous avez quitté le chat d'équipe.");
     }
 
     @Override
     public void update(String message) {
-        System.out.println(name + " a reçu un nouveau message: " + message);
+        System.out.println(name + ": " + message);
     }
 }
