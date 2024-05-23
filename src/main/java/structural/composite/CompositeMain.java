2831093e9d82a549109fbfc37efa496c51c0c389
Erwan <erwanletutour.elt@gmail.com>
2024-05-23 14:35:43 +0200

    composite

--- src/main/java/structural/composite/CompositeMain.java
+++ src/main/java/structural/composite/CompositeMain.java
@@ -2,26 +2,6 @@ package structural.composite;
 
 public class CompositeMain {
     public static void main(String[] args) {
-        /*Personne philippe = new Individu("Philippe", 62, "Homme");
-        Personne isabelle = new Individu("Isabelle", 57, "Femme");
-        Famille grandsParents = new Famille(philippe, isabelle);
-
-        Personne erwan = new Individu("Erwan", 33, "Homme");
-        Personne amelie = new Individu("Amelie", 34, "Femme");
-        Famille parent = new Famille(erwan, amelie);
-        Personne mathys = new Individu("Mathys", 5, "Homme");
-
-        Personne alan = new Individu("Alan", 30, "Homme");
-        Personne katell = new Individu("Katell", 27, "Femme");
-
-        grandsParents.ajouterEnfant(parent);
-        grandsParents.ajouterEnfant(alan);
-        grandsParents.ajouterEnfant(katell);
-
-        parent.ajouterEnfant(mathys);
-
-        System.out.println("Arbre généalogique des grands-parents:");
-        grandsParents.afficher(0);*/
         Personne philippe = new Individu("Philippe", 62, "Homme");
         Personne isabelle = new Individu("Isabelle", 57, "Femme");
         Famille grandsParents = new Famille(philippe, isabelle);
